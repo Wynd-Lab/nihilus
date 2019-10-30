5322a60a84b646b23dbe4ca3e81a8e449cfbf2df
spontoreau <pontoreau.sylvain@gmail.com>
2019-10-30 14:16:20 +0100

    :white_check_mark: Update QueryBus tests

--- tests/QueryBusTest.php
+++ tests/QueryBusTest.php
@@ -1,6 +1,9 @@
 <?php
 
+namespace Nihilus\Tests;
+
 use Nihilus\QueryBus;
+use Nihilus\QueryBusInterface;
 use Nihilus\QueryHandlerInterface;
 use Nihilus\QueryHandlerResolverInterface;
 use Nihilus\QueryInterface;
@@ -16,125 +19,26 @@ use PHPUnit\Framework\TestCase;
 final class QueryBusTest extends TestCase
 {
     /**
-     * @var QueryInterface
+     * @var QueryBusTestContext
      */
-    private $query;
+    private $context;
 
     /**
-     * @var object
+     * @var QueryBusInterface
      */
-    private $queryResult;
-
-    /**
-     * @var QueryHandlerInterface
-     */
-    private $queryHandler;
-
-    /**
-     * @var QueryHandlerInterface
-     */
-    private $queryHandlerResolverReturn;
-
-    /**
-     * @var QueryHandlerResolverInterface
-     */
-    private $queryHandlerResolver;
-
-    /**
-     * @var QueryMiddlewareResolverInterface
-     */
-    private $queryMiddlewareResolver;
-
-    /**
-     * @var QueryMiddlewareInterface[]
-     */
-    private $queryMiddlewareResolverReturn;
-
-    /**
-     * @var QueryMiddlewareInterface
-     */
-    private $queryMiddleware;
+    private $queryBus;
 
     public function setUp()
     {
-        $uid = uniqid();
-        $this->queryResult = new class($uid) {
-            /**
-             * @var string
-             */
-            private $value;
-
-            public function __construct(string $value)
-            {
-                $this->value = $value;
-            }
-
-            public function getValue(): string
-            {
-                return $this->result;
-            }
-        };
-
-        $this->query = new class($uid) implements QueryInterface {
-            private $prop;
-
-            public function __construct(string $value)
-            {
-                $this->prop = $value;
-            }
-
-            public function getProp(): string
-            {
-                return $this->prop;
-            }
-        };
-
-        $this->queryHandler = $this
-            ->getMockBuilder(QueryHandlerInterface::class)
-            ->setMethods(['handle'])
-            ->getMock()
-        ;
-
-        $this->queryHandlerResolver = $this
-            ->getMockBuilder(QueryHandlerResolverInterface::class)
-            ->setMethods((['get']))
-            ->getMock()
-        ;
-
-        $this->queryHandlerResolver
-            ->method('get')
-            ->will($this->returnCallback(
-                function () {
-                    return $this->queryHandlerResolverReturn;
-                }
-            ))
-        ;
-
-        $this->queryHandlerResolverReturn = $this->queryHandler;
-
-        $this->queryMiddleware = $this
-            ->getMockBuilder(QueryMiddlewareInterface::class)
-            ->setMethods(['handle'])
-            ->getMock()
-        ;
-
-        $this->queryMiddlewareResolver = $this
-            ->getMockBuilder(QueryMiddlewareResolverInterface::class)
-            ->setMethods((['get']))
-            ->getMock()
-        ;
-
-        $this->queryMiddlewareResolver
-            ->method('get')
-            ->with($this->query)
-            ->will($this->returnCallback(
-                function () {
-                    return $this->queryMiddlewareResolverReturn;
-                }
-            ))
-        ;
-
-        $this->queryMiddlewareResolverReturn = [];
+        $this->context = new QueryBusTestContext($this);
+        $this->context->setUpQueryResult();
+        $this->context->setUpQuery();
+        $this->context->setUpHandler();
+        $this->context->setUpMiddlewares();
+        $this->queryBus = new QueryBus(
+            $this->context->getQueryHandlerResolver(), 
+            $this->context->getQueryMiddlewareResolver()
+        );
     }
 
     /**
@@ -143,19 +47,12 @@ final class QueryBusTest extends TestCase
     public function Given_AQueryBusWithHandlers_When_AQueryIsExecuted_Then_AHandlerReturnSomething()
     {
         // Arrange
-        $expected = $this->queryResult;
-
-        $this->queryHandler
-            ->method('handle')
-            ->with($this->query)
-            ->willReturn($expected)
-        ;
-
-        $queryBus = new QueryBus($this->queryHandlerResolver, $this->queryMiddlewareResolver);
+        $expected = $this->context->getQueryResult();
 
         // Act
-        $actual = $queryBus->execute($this->query);
+        $actual = $this->queryBus->execute($this->context->getQuery());
 
+        // Assert
         $this->assertEquals($actual, $expected);
     }
 
@@ -165,26 +62,19 @@ final class QueryBusTest extends TestCase
     public function Given_AQueryBusWithHandlers_When_AQueryIsExecuted_Then_AHandlerHandleItOnce()
     {
         // Arrange
-        $expected = $this->queryResult;
-
-        $this->queryHandlerResolver
-            ->method('get')
-            ->with($this->query)
-            ->willReturn($this->queryHandler)
-        ;
-
-        $queryBus = new QueryBus($this->queryHandlerResolver, $this->queryMiddlewareResolver);
+        $expected = $this->context->getQueryResult();
 
         // Assert
-        $this->queryHandler
+        $this->context
+            ->getQueryHandler()
             ->expects($this->once())
             ->method('handle')
-            ->with($this->query)
+            ->with($this->context->getQuery())
             ->willReturn($expected)
         ;
 
         // Act
-        $queryBus->execute($this->query);
+        $this->queryBus->execute($this->context->getQuery());
     }
 
     /**
@@ -193,23 +83,15 @@ final class QueryBusTest extends TestCase
     public function Given_AQueryBusWithoutHandlers_When_AQueryIsExecuted_Then_AnUnkowQueryExceptionIsThrow()
     {
         // Arrange
-        $this->queryHandlerResolverReturn = null;
         $query = new class() implements QueryInterface {
+
         };
-
-        $this->queryHandlerResolver
-            ->method('get')
-            ->with($query)
-            ->willReturn(null)
-        ;
-
-        $queryBus = new QueryBus($this->queryHandlerResolver, $this->queryMiddlewareResolver);
 
         // Assert
         $this->expectException(UnknowQueryException::class);
 
         // Act
-        $queryBus->execute($query);
+        $this->queryBus->execute($query);
     }
 
     /**
@@ -217,31 +99,16 @@ final class QueryBusTest extends TestCase
      */
     public function Given_AQueryBusWithHandlersAndMiddlewares_When_AQueryIsExecuted_Then_MiddlewaresAreExecuted()
     {
-        // Arrange
-        $this->queryMiddlewareResolverReturn = [$this->queryMiddleware];
-        $queryBus = new QueryBus($this->queryHandlerResolver, $this->queryMiddlewareResolver);
-
-        $this->queryHandlerResolver
-            ->method('get')
-            ->with($this->query)
-            ->willReturn($this->queryHandler)
-        ;
-
-        $this->queryHandler
-            ->method('handle')
-            ->with($this->query)
-            ->willReturn($this->queryResult)
-        ;
-
         // Assert
-        $this->queryMiddleware
+        $this->context
+            ->getQueryMiddleware()
             ->expects($this->once())
             ->method('handle')
-            ->with($this->query)
+            ->with($this->context->getQuery())
         ;
 
         // Act
-        $queryBus->execute($this->query);
+        $this->queryBus->execute($this->context->getQuery());
     }
 
     /**
@@ -249,36 +116,16 @@ final class QueryBusTest extends TestCase
      */
     public function Given_AQueryBusWithHandlersAndMiddlewares_When_AQueryIsExecuted_Then_AHandlerHandledIt()
     {
-        // Arrange
-        $this->queryMiddlewareResolverReturn = [new class() implements QueryMiddlewareInterface {
-            public function handle(QueryInterface $query, QueryHandlerInterface $next): object
-            {
-                return $next->handle($query);
-            }
-        }];
-
-        $this->queryHandlerResolver
-            ->method('get')
-            ->with($this->query)
-            ->willReturn($this->queryHandler)
-        ;
-
-        $this->queryHandler
-            ->method('handle')
-            ->with($this->query)
-            ->willReturn($this->queryResult)
-        ;
-
-        $queryBus = new QueryBus($this->queryHandlerResolver, $this->queryMiddlewareResolver);
-
-        $this->queryHandler
+        // Assert
+        $this->context
+            ->getQueryHandler()
             ->expects($this->once())
             ->method('handle')
-            ->with($this->query)
+            ->with($this->context->getQuery())
         ;
 
         // Act
-        $queryBus->execute($this->query);
+        $this->queryBus->execute($this->context->getQuery());
     }
 
     /**
@@ -287,28 +134,22 @@ final class QueryBusTest extends TestCase
     public function Given_AQueryBusWithHandlersAndMiddlewaresThatDontCallNextMiddleware_When_AQueryIsExecuted_Then_NoHandlerHandledIt()
     {
         // Arrange
-        $this->queryMiddlewareResolverReturn = [new class() implements QueryMiddlewareInterface {
+        $this->context->addMiddleware(new class() implements QueryMiddlewareInterface {
             public function handle(QueryInterface $query, QueryHandlerInterface $next): object
             {
                 return new class() {
                 };
             }
-        }];
-        $queryBus = new QueryBus($this->queryHandlerResolver, $this->queryMiddlewareResolver);
+        });
 
-        $this->queryHandlerResolver
-            ->method('get')
-            ->with($this->query)
-            ->willReturn($this->queryHandler)
-        ;
-
-        $this->queryHandler
+        $this->context
+            ->getQueryHandler()
             ->expects($this->never())
             ->method('handle')
-            ->with($this->query)
+            ->with($this->context->getQuery())
         ;
 
         // Act
-        $queryBus->execute($this->query);
+        $this->queryBus->execute($this->context->getQuery());
     }
 }
